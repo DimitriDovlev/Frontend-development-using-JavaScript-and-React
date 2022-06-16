9c629d96e22f1996181368c32cc7d2a80b6fcd08
DimitriDovlev <dimitri.dovlev2000@gmail.com>
2022-06-16 14:33:46 +0200

    Class_1 Code

---
+++ Class_01/our-first-react-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header';
+import ShopMenu from './ShopMenu';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ShopMenu />
+      
+    </div>
+  );
+}
+
+export default App;
